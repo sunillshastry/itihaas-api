04b5b16715e703f1ba4147076441e8ae699189d6
sunil <sunil.shastry69@gmail.com>
2025-03-27 01:12:31 -0600

    Setup router for dynasties endpoint in routes/dynasties.js source file

---
+++ routes/dynasties.js
@@ -0,0 +1,11 @@
+// Package imports
+const express = require('express');
+
+const router = express.Router();
+
+// Routes
+// GET Routes
+router.get('/');
+
+// Router export
+module.exports = router;
