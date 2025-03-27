61c0d31c4cca47c9622647f5b5c148bbbe2c28f6
sunil <sunil.shastry69@gmail.com>
2025-03-27 01:12:48 -0600

    Integrate dynasties router to index.js file

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+// Package imports
 const express = require('express');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
+// Local imports
+const dynastiesRouter = require('./routes/dynasties');
 const createDatabaseConnection = require('./database/config');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+// Routing
+app.use('/api/v1/dynasties', dynastiesRouter);
 
 app.listen(PORT, () => {
   if (process.env.NODE_ENV === 'development') {
