b98fe28d76b599eb54f227a5438373632bbdabb6
sunil <sunil.shastry69@gmail.com>
2025-03-27 00:26:44 -0600

    Add initial GET route at root endpoint

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.get('/', function (request, response) {
+  return response.status(200).json({
+    success: true,
+    message: 'Welcome to Itihaas API!',
+  });
+});
+
 app.listen(PORT, () => {
   if (process.env.NODE_ENV === 'development') {
     console.log(`Server currently running on http://localhost:${PORT}`);
