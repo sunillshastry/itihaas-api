b600c9e3290777e9c219b232acf817b5bba1adc2
sunil <sunil.shastry69@gmail.com>
2025-03-27 01:07:23 -0600

    Incorporate createDatabaseConnection function in index.js source file

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const createDatabaseConnection = require('./database/config');
+
 const app = express();
 const PORT = process.env.PORT || 8000;
-
-app.get('/', function (request, response) {
-  return response.status(200).json({
-    success: true,
-    message: 'Welcome to Itihaas API!',
-  });
-});
 
 app.listen(PORT, () => {
   if (process.env.NODE_ENV === 'development') {
     console.log(`Server currently running on http://localhost:${PORT}`);
   }
 });
+createDatabaseConnection();
