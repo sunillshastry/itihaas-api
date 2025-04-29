ff0464b73ff0ce50e4356962f8d96b007ae037db
sunil <sunil.shastry69@gmail.com>
2025-04-28 22:50:34 -0600

    Develop getDynastyBySlugName controller function for /dynasties/slug/ route

--- controllers/dynasties.js
+++ controllers/dynasties.js
@@ -75,7 +75,45 @@ async function getDynastiesById(request, response) {
   }
 }
 
+/**
+ * Controller function to get a dynasty by its slug name for dynasties route
+ *
+ * @param {Object} request Default Express Request object
+ * @param {Object} response Default Express Response object
+ * @returns A response with status code 200, 404 or 500 signifying success or failure respectively
+ */
+async function getDynastyBySlugName(request, response) {
+  const { slug } = request.params;
+  try {
+    // Get dynasty from database via its slug
+    const dynasty = await Dynasties.findOne({ slug });
+
+    // Check if dynasty does not exist - return 404
+    if (!dynasty) {
+      return response.status(404).json({
+        success: false,
+        message: FailureLogs.entityNotFound(),
+      });
+    }
+
+    // Return success response
+    return response.status(200).json({
+      success: true,
+      data: {
+        dynasty,
+      },
+    });
+  } catch (e) {
+    return response.status(500).json({
+      success: false,
+      message: FailureLogs.databaseAccessFailure(),
+      errorLog: e,
+    });
+  }
+}
+
 module.exports = {
   getAllDynasties,
   getDynastiesById,
+  getDynastyBySlugName,
 };
