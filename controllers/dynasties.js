eefd1dbdc7ad24b05885832ec398841872f85676
sunil <sunil.shastry69@gmail.com>
2025-04-28 22:42:26 -0600

    Implement getDynastyById controller function for /dynasties/id/ route endpoint

--- controllers/dynasties.js
+++ controllers/dynasties.js
@@ -38,6 +38,44 @@ async function getAllDynasties(request, response) {
   }
 }
 
+/**
+ * Controller function to get a dynasty by its ID parameter for dynasties route
+ *
+ * @param {Object} request Default Express Request object
+ * @param {Object} response Default Express Response object
+ * @returns A response with status code 200, 404 or 500 signifying success or failures respectively
+ */
+async function getDynastiesById(request, response) {
+  const { id } = request.params;
+  try {
+    // Get dynasty from database
+    const dynasty = await Dynasties.findById(id);
+
+    // Check if dynasty exists - send 404 response if it doesn't
+    if (!dynasty) {
+      return response.status(404).json({
+        success: false,
+        message: FailureLogs.entityNotFound(),
+      });
+    }
+
+    // Return success response
+    return response.status(200).json({
+      success: true,
+      data: {
+        dynasty,
+      },
+    });
+  } catch (e) {
+    return response.status(500).json({
+      success: false,
+      message: FailureLogs.databaseAccessFailure(),
+      errorLog: e,
+    });
+  }
+}
+
 module.exports = {
   getAllDynasties,
+  getDynastiesById,
 };
