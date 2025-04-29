05ef214742747aeda02705f289ecd02224e2f442
sunil <sunil.shastry69@gmail.com>
2025-04-28 22:36:46 -0600

    Create new FailureLog method - entityNotFound function

--- services/FailureLogs.js
+++ services/FailureLogs.js
@@ -6,6 +6,14 @@ class FailureLogs {
   static databaseAccessFailure() {
     return 'Fail: Unable to access specified information from database due to an internal server error';
   }
+
+  /**
+   * Failure log when the user requests for an entity that is absent in the database
+   * @returns A string consisting of the failure message
+   */
+  static entityNotFound() {
+    return 'Fail: Unable to find the entity under the specified identifier parameter';
+  }
 }
 
 module.exports = FailureLogs;
