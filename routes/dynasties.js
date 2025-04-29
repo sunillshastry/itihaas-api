ac7424a02b247cdb526d8c92f1777bdb978a0f80
sunil <sunil.shastry69@gmail.com>
2025-04-28 22:50:04 -0600

    Include new route for dynasties: /dynasties/slug and attach its controller function to it

--- routes/dynasties.js
+++ routes/dynasties.js
@@ -5,12 +5,14 @@ const router = express.Router();
 const {
   getAllDynasties,
   getDynastiesById,
+  getDynastyBySlugName,
 } = require('../controllers/dynasties');
 
 // Routes
 // GET Routes
 router.get('/', getAllDynasties);
 router.get('/id/:id', getDynastiesById);
+router.get('/:slug', getDynastyBySlugName);
 
 // Router export
 module.exports = router;
