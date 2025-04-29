774906c5f14ac103989ee500976ece04e776d2c5
sunil <sunil.shastry69@gmail.com>
2025-04-28 22:22:57 -0600

    Update createDatabaseConnection function to handle logic better with dev/prod checks and better logging

--- database/config.js
+++ database/config.js
@@ -3,22 +3,32 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const DATABASE_CONNECTION_URI = process.env.MONGO_DB_URI;
-
 /**
  * Create a connection to the database for backend interactions
  */
 async function createDatabaseConnection() {
   try {
-    // Checking for a successful connection and display message on development mode only
-    const connection = await mongoose.connect(DATABASE_CONNECTION_URI);
-    if (connection && process.env.NODE_ENV === 'development') {
-      console.log('Created connection to database successfully');
+    if (process.env.MONGO_DB_CONNECTION_STRING) {
+      // Checking for a successful connection and display message on development mode only
+      const connection = await mongoose.connect(
+        process.env.MONGO_DB_CONNECTION_STRING,
+      );
+      if (connection && process.env.NODE_ENV === 'development') {
+        console.log(
+          `Successfully established connection to database: ${connection.connection.host}`,
+        );
+      }
+    } else {
+      console.log(
+        'Server unable to establish connection to database due to absence of database connection string',
+      );
     }
   } catch (e) {
     // Display failure message on development mode only
+    console.log(
+      'Failed to create connection to database due to an internal server error',
+    );
     if (process.env.NODE_ENV === 'development') {
-      console.log('Failed to connect to MongoDB database');
       console.log(`Error Log: ${e}`);
     }
   }
