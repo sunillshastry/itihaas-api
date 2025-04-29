79d651f24db45748acf2e6d4b367e8b6ac200a1e
sunil <sunil.shastry69@gmail.com>
2025-04-28 22:31:39 -0600

    Add new route for dynasties: /dynasties/id/ with controller function getDynastyById

--- routes/dynasties.js
+++ routes/dynasties.js
@@ -2,11 +2,15 @@
 const express = require('express');
 
 const router = express.Router();
-const { getAllDynasties } = require('../controllers/dynasties');
+const {
+  getAllDynasties,
+  getDynastiesById,
+} = require('../controllers/dynasties');
 
 // Routes
 // GET Routes
 router.get('/', getAllDynasties);
+router.get('/id/:id', getDynastiesById);
 
 // Router export
 module.exports = router;
