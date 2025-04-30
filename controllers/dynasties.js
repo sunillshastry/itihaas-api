b1c5207019e78092c1bff071309c0719c2d794fe
sunil <sunil.shastry69@gmail.com>
2025-04-30 15:49:35 -0600

    Author getDynastyByTitles controller function for /dynasties/search/titles route endpoint. Tested and documented

--- controllers/dynasties.js
+++ controllers/dynasties.js
@@ -152,8 +152,84 @@ async function getDynastyBySlugName(request, response) {
   }
 }
 
+/**
+ * Controller function to get a list of dynasty names with specified 'id' or 'slug' fields
+ *
+ * @param {Object} request Default Express request object
+ * @param {Object} response Default Express response object
+ * @returns A response code with status code 200, 404 or 500 signifying success or failure respectively
+ */
+async function getDynastyTitles(request, response) {
+  // Retrieve the only query
+  const { include } = request.query;
+
+  // Format query fields from the user
+  const userRequestedFields =
+    (include &&
+      include.split(',').map((field) => field.trim().toLowerCase())) ||
+    [];
+
+  // List of valid query entry fields
+  const VALID_FIELD_ENTRIES = ['id', 'slug'];
+
+  const databaseProjection = { name: 1, _id: 0 };
+  let isReturnTypeObject = false;
+
+  // Check if query is included and validate them
+  try {
+    if (include) {
+      const prohibitedFields = userRequestedFields.filter(
+        (field) => !VALID_FIELD_ENTRIES.includes(field),
+      );
+      if (prohibitedFields.length > 0) {
+        return response.status(404).json({
+          success: false,
+          message: FailureLogs.entityNotFound(),
+        });
+      }
+
+      isReturnTypeObject = true;
+      // Update _id field in projection
+      if (userRequestedFields.includes('id')) {
+        // eslint-disable-next-line no-underscore-dangle
+        databaseProjection._id = 1;
+      }
+
+      // Update slug field in projection
+      if (userRequestedFields.includes('slug')) {
+        databaseProjection.slug = 1;
+      }
+    }
+
+    // Fetch data
+    const dynasties = await Dynasties.find({}, databaseProjection).lean();
+    let result;
+    if (isReturnTypeObject) {
+      result = dynasties;
+    } else {
+      result = dynasties.map((dynasty) => dynasty.name);
+    }
+
+    // Return success response
+    return response.status(200).json({
+      success: true,
+      size: result.length,
+      data: {
+        dynasties: result,
+      },
+    });
+  } catch (e) {
+    return response.status(500).json({
+      success: false,
+      message: FailureLogs.databaseAccessFailure(),
+      errorLog: e,
+    });
+  }
+}
+
 module.exports = {
   getAllDynasties,
   getDynastiesById,
   getDynastyBySlugName,
+  getDynastyTitles,
 };
