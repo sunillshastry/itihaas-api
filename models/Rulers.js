d5530a0f6439b707dc32fa7859b1c596db9016d2
sunil <sunil.shastry69@gmail.com>
2025-05-13 05:42:01 -0600

    Include timestamps option in models/Rulers.js source file

--- models/Rulers.js
+++ models/Rulers.js
@@ -1,174 +1,177 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  slug: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    required: true,
-    unique: false,
-  },
-  otherNames: {
-    type: [String],
-    required: false,
-    unique: false,
-    default: [],
-  },
-  born: {
-    type: String,
-    required: false,
-    unique: false,
-    default: null,
-  },
-  died: {
-    type: String,
-    required: false,
-    unique: false,
-    default: null,
-  },
-  dynasty: {
-    type: String,
-    required: false,
-    unique: false,
-    default: null,
-  },
-  religion: {
-    type: String,
-    required: false,
-    unique: false,
-    default: null,
-  },
-  predecessor: {
-    type: String,
-    required: false,
-    unique: false,
-    default: null,
-  },
-  successor: {
-    type: String,
-    required: false,
-    unique: false,
-    default: null,
-  },
-  family: {
-    father: {
+const schema = new mongoose.Schema(
+  {
+    slug: {
       type: String,
-      required: false,
-      unique: false,
-      default: null,
+      required: true,
+      unique: true,
     },
-    mother: {
+    name: {
       type: String,
-      required: false,
+      required: true,
       unique: false,
-      default: null,
     },
-    wives: {
+    otherNames: {
       type: [String],
       required: false,
       unique: false,
       default: [],
     },
-    children: {
-      type: [String],
-      required: false,
-      unique: false,
-      default: [],
-    },
-  },
-  wars: {
-    type: [String],
-    required: false,
-    unique: false,
-    default: [],
-  },
-  timeline: {
-    begin: {
+    born: {
       type: String,
       required: false,
       unique: false,
       default: null,
     },
-    end: {
+    died: {
       type: String,
       required: false,
       unique: false,
       default: null,
     },
-  },
-  description: {
-    oneline: {
+    dynasty: {
       type: String,
       required: false,
       unique: false,
       default: null,
     },
-    long: {
-      type: [String],
+    religion: {
+      type: String,
       required: false,
       unique: false,
-      default: [],
+      default: null,
     },
-  },
-  sources: {
-    type: [String],
-    required: false,
-    unique: false,
-    default: [],
-  },
-  furtherReading: {
-    type: [
-      {
-        publisher: {
-          type: String,
-          required: false,
-          unique: false,
-          default: null,
-        },
-        link: {
-          type: String,
-          required: false,
-          unique: false,
-          default: null,
-        },
+    predecessor: {
+      type: String,
+      required: false,
+      unique: false,
+      default: null,
+    },
+    successor: {
+      type: String,
+      required: false,
+      unique: false,
+      default: null,
+    },
+    family: {
+      father: {
+        type: String,
+        required: false,
+        unique: false,
+        default: null,
       },
-    ],
-  },
-  articles: {
-    type: [
-      {
-        title: {
-          type: String,
-          required: false,
-          unique: false,
-          default: null,
-        },
-        authors: {
-          type: [String],
-          required: false,
-          unique: false,
-          default: [],
-        },
-        publisher: {
-          type: String,
-          required: false,
-          unique: false,
-          default: null,
-        },
-        link: {
-          type: String,
-          required: false,
-          unique: false,
-          default: null,
-        },
+      mother: {
+        type: String,
+        required: false,
+        unique: false,
+        default: null,
       },
-    ],
-    default: [],
+      wives: {
+        type: [String],
+        required: false,
+        unique: false,
+        default: [],
+      },
+      children: {
+        type: [String],
+        required: false,
+        unique: false,
+        default: [],
+      },
+    },
+    wars: {
+      type: [String],
+      required: false,
+      unique: false,
+      default: [],
+    },
+    timeline: {
+      begin: {
+        type: String,
+        required: false,
+        unique: false,
+        default: null,
+      },
+      end: {
+        type: String,
+        required: false,
+        unique: false,
+        default: null,
+      },
+    },
+    description: {
+      oneline: {
+        type: String,
+        required: false,
+        unique: false,
+        default: null,
+      },
+      long: {
+        type: [String],
+        required: false,
+        unique: false,
+        default: [],
+      },
+    },
+    sources: {
+      type: [String],
+      required: false,
+      unique: false,
+      default: [],
+    },
+    furtherReading: {
+      type: [
+        {
+          publisher: {
+            type: String,
+            required: false,
+            unique: false,
+            default: null,
+          },
+          link: {
+            type: String,
+            required: false,
+            unique: false,
+            default: null,
+          },
+        },
+      ],
+    },
+    articles: {
+      type: [
+        {
+          title: {
+            type: String,
+            required: false,
+            unique: false,
+            default: null,
+          },
+          authors: {
+            type: [String],
+            required: false,
+            unique: false,
+            default: [],
+          },
+          publisher: {
+            type: String,
+            required: false,
+            unique: false,
+            default: null,
+          },
+          link: {
+            type: String,
+            required: false,
+            unique: false,
+            default: null,
+          },
+        },
+      ],
+      default: [],
+    },
   },
-});
+  { timestamps: true },
+);
 
 const model = mongoose.model('Ruler', schema);
 
