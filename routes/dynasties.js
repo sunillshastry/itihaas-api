6d53ca6e38d2c3beb86da831786af4faa50ab418
sunil <sunil.shastry69@gmail.com>
2025-06-22 13:53:38 -0600

    Create new route(s) for dynasties with controller functions

--- routes/dynasties.js
+++ routes/dynasties.js
@@ -7,14 +7,20 @@ const {
   getDynastiesById,
   getDynastyBySlugName,
   getDynastyTitles,
+  getDynastiesBySearch,
+  getRandomDynasty,
 } = require('../controllers/dynasties');
 
 // Routes
 // GET Routes
 router.get('/', getAllDynasties);
+router.get('/search/titles', getDynastyTitles);
+router.get('/random', getRandomDynasty);
+
+// GET routes (with params)
+router.get('/s/:search', getDynastiesBySearch);
 router.get('/id/:id', getDynastiesById);
 router.get('/:slug', getDynastyBySlugName);
-router.get('/search/titles', getDynastyTitles);
 
 // Router export
 module.exports = router;
