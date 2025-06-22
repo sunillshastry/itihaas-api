9acfd6f83792592fede4aa66345d130bae3c2703
sunil <sunil.shastry69@gmail.com>
2025-06-22 13:37:11 -0600

    Develop getRulersBySearch controller function for newly created ruler route endpoint

--- controllers/rulers.js
+++ controllers/rulers.js
@@ -414,10 +414,67 @@ async function getRandomRuler(request, response) {
   }
 }
 
+/**
+ * Controller function to get rulers by search
+ *
+ * @param {Object} request Default Express request object
+ * @param {Object} response Default Express response object
+ * @returns A response code with status code 200, 404 or 500 signifying success or failure respectively
+ */
+async function getRulersBySearch(request, response) {
+  const { search } = request.params;
+  try {
+    // Use MongoDB find query to get all matches with 'name' or 'otherNames' properties
+    const rulers = await Rulers.find({
+      $or: [
+        { name: { $regex: search, $options: 'i' } },
+        { otherNames: { $elemMatch: { $regex: search, $options: 'i' } } },
+      ],
+    });
+
+    // Return success response
+    return response.status(200).json({
+      success: true,
+      size: rulers.length,
+      data: {
+        rulers,
+      },
+    });
+  } catch (e) {
+    // Build app exception on server error
+    const appException = new AppException(
+      'Failed to fetch specified resource from the database',
+      500,
+      'fail',
+      false,
+      `${request.host}:${request.originalUrl}`,
+      request.method,
+      'controllers.dynasties.getRulersBySearch',
+    );
+
+    // Development mode response
+    if (process.env.NODE_ENV === 'development') {
+      return response.status(500).json({
+        success: false,
+        message: FailureLogs.databaseAccessFailure(),
+        errorLog: e,
+        log: appException.log(),
+      });
+    }
+
+    // Default response
+    return response.status(500).json({
+      success: false,
+      message: FailureLogs.databaseAccessFailure(),
+    });
+  }
+}
+
 module.exports = {
   getAllRulers,
   getRulerById,
   getRulerBySlugName,
   getRulerTitles,
   getRandomRuler,
+  getRulersBySearch,
 };
