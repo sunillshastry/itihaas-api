78644d95e47ab0997d5e32b42d768cd69f2b9373
sunil <sunil.shastry69@gmail.com>
2025-07-15 00:31:37 -0600

    Add documentation

--- controllers/version/getApiVersion.js
+++ controllers/version/getApiVersion.js
@@ -1,5 +1,11 @@
 const packageVersion = require('@/package.json');
 
+/**
+ * Get app version data of the Itihaas API
+ * @param {object} request Default Express request object
+ * @param {object} response Default Express response object
+ * @returns An API Version information response with status code 200
+ */
 async function getApiVersion(request, response) {
   return response.status(200).json({
     app: 'itihaas-api',
