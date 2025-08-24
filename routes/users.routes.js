70e9dc41ee7c0dfee90151007e3b1c994d88052f
sunil <sunil.shastry69@gmail.com>
2025-08-24 05:02:19 -0600

    Setup three routes for users router: /verify, /register and /recover with their controller functions except /recover

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,9 +1,12 @@
 // Package imports
 const apiRegistration = require('@/controllers/users/apiRegistration.controller');
+const apiVerify = require('@/controllers/users/apiVerify.controller');
 const express = require('express');
 
 const router = express.Router({ strict: false });
 
+router.post('/verify', apiVerify);
 router.post('/register', apiRegistration);
+router.post('/recover');
 
 module.exports = router;
