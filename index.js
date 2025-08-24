bf5056ee984a6b4b2f9c9fae72b21d02c48e6607
sunil <sunil.shastry69@gmail.com>
2025-08-24 02:19:39 -0600

    Remove unwanted imports and redundant code and use imported 'app' module and listen to server

--- index.js
+++ index.js
@@ -1,49 +1,19 @@
 // Package imports
 require('module-alias/register');
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 // Local imports
-const dynastiesRouter = require('@/routes/dynasties.routes');
-const rulersRouter = require('@/routes/rulers.routes');
 const createDatabaseConnection = require('@/database/config');
-const healthCheckRouter = require('@/routes/healthcheck.routes');
-const versionRouter = require('@/routes/version.routes');
-const infoRouter = require('@/routes/info.routes');
-const baseRouter = require('@/routes/base.routes');
+const app = require('@/config/server');
 
-const app = express();
 const HOST = process.env.SERVER_HOST;
 const PORT = process.env.SERVER_PORT;
 
-// Third-party middlewares
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(helmet());
-
-// Routing
-app.use('/', baseRouter);
-app.use('/api/v1/dynasties', dynastiesRouter);
-app.use('/api/v1/rulers', rulersRouter);
-app.use('/api/health', healthCheckRouter);
-app.use('/api/version', versionRouter);
-app.use('/api/info', infoRouter);
-
 if (HOST && PORT) {
   app.listen(PORT, HOST, function () {
-    if (process.env.NODE_ENV === 'development') {
-      console.log(`Server currently running on http://${HOST}:${PORT}/`);
-    }
+    console.log(`Server currently running on http://${HOST}:${PORT}/`);
     createDatabaseConnection();
   });
 } else {
