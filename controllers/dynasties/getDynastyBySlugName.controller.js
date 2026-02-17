18b3cb01053007c7ce3fde501cfaa1daaca2ea13
Sunil Shastry <sunil.shastry69@gmail.com>
2026-02-16 18:34:28 -0600

    Remove the Redis caching mechanism from getDynastyBySlugName controller function

--- controllers/dynasties/getDynastyBySlugName.controller.js
+++ controllers/dynasties/getDynastyBySlugName.controller.js
@@ -9,7 +9,7 @@ const Dynasties = require('@/models/Dynasties');
 // Services
 const FailureLogs = require('@/services/FailureLogs');
 const AppException = require('@/services/AppException');
-const redisClient = require('@/cache/ioRedisConfig');
+// const redisClient = require('@/cache/ioRedisConfig');
 
 /**
  * Controller function to get a dynasty by its slug name for dynasties route
@@ -22,25 +22,28 @@ const redisClient = require('@/cache/ioRedisConfig');
 async function getDynastyBySlugName(request, response) {
   const { slug } = request.params;
   try {
+    // WARNING: Removing/Commenting out the entire Redis cache check due to arising problems at the moment
+    // TODO: Implement a better caching strategy with Redis and refactor or update this code segment in time.
     // Check for cached 'slug' key data
-    const cacheKeyId = `dynasty:slug:${slug}`;
+    // const cacheKeyId = `dynasty:slug:${slug}`;
 
     // If cached slug key exists
-    if (cacheKeyId) {
-      // Get cached dynasty data via the 'id' property
-      const idKey = `dynasty:${cacheKeyId}`;
-      const cacheData = await redisClient.get(idKey);
+    // WARNING: Removing/Commenting out the entire Redis cache check due to arising problems at the moment
+    // if (cacheKeyId) {
+    //   // Get cached dynasty data via the 'id' property
+    //   const idKey = `dynasty:${cacheKeyId}`;
+    //   const cacheData = await redisClient.get(idKey);
 
-      // If any cached dynasty data exists from the 'id' property
-      if (cacheData) {
-        return response.status(200).json({
-          success: true,
-          data: {
-            dynasty: JSON.parse(cacheData),
-          },
-        });
-      }
-    }
+    //   // If any cached dynasty data exists from the 'id' property
+    //   if (cacheData) {
+    //     return response.status(200).json({
+    //       success: true,
+    //       data: {
+    //         dynasty: JSON.parse(cacheData),
+    //       },
+    //     });
+    //   }
+    // }
 
     // No cache hits:
     // Get dynasty from database via its slug
@@ -49,7 +52,8 @@ async function getDynastyBySlugName(request, response) {
     // Check if dynasty does not exist - return 404
     if (!dynasty) {
       // Delete cache key
-      await redisClient.del(cacheKeyId);
+      // WARNING: Removing/Commenting out the entire Redis cache check due to arising problems at the moment
+      // await redisClient.del(cacheKeyId);
 
       // Build AppException
       const appException = new AppException(
@@ -80,22 +84,23 @@ async function getDynastyBySlugName(request, response) {
     // Set retrieved data from database to cache system
     // First: Set actual dynasty content via dynasty._id value key
     // eslint-disable-next-line no-underscore-dangle
-    const idKey = `dynasty:${dynasty._id}`;
-    await redisClient.set(
-      idKey,
-      JSON.stringify(dynasty),
-      'EX',
-      process.env.UPSTASH_REDIS_TTL_DURATION,
-    );
+    // WARNING: Removing/Commenting out the entire Redis cache check due to arising problems at the moment
+    // const idKey = `dynasty:${dynasty._id}`;
+    // await redisClient.set(
+    //   idKey,
+    //   JSON.stringify(dynasty),
+    //   'EX',
+    //   process.env.UPSTASH_REDIS_TTL_DURATION,
+    // );
 
-    // Second: Set dynasty._id value via dynasty:slug key
-    await redisClient.set(
-      cacheKeyId,
-      // eslint-disable-next-line no-underscore-dangle
-      dynasty._id.toString(),
-      'EX',
-      process.env.UPSTASH_REDIS_TTL_DURATION,
-    );
+    // // Second: Set dynasty._id value via dynasty:slug key
+    // await redisClient.set(
+    //   cacheKeyId,
+    //   // eslint-disable-next-line no-underscore-dangle
+    //   dynasty._id.toString(),
+    //   'EX',
+    //   process.env.UPSTASH_REDIS_TTL_DURATION,
+    // );
 
     // Return success response
     return response.status(200).json({
