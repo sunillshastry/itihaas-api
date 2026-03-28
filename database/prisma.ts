61da23afcdfc21751f0e948507f1f6c73478a11a
Sunil Shastry <sunil.shastry69@gmail.com>
2026-03-27 22:24:56 -0600

    Include the datasource URL in the PrismaClient configuration

--- database/prisma.ts
+++ database/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env?.['NEON_DATABASE_CONNECTION_STRING'] as string,
+    },
+  },
+});
 
 export default prisma;
