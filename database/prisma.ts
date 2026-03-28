02a51d9832008d023875e40aedeb79e071d0569f
Sunil Shastry <sunil.shastry69@gmail.com>
2026-03-27 22:28:28 -0600

    Revert prisma changes due to current usage version

--- database/prisma.ts
+++ database/prisma.ts
@@ -1,11 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env?.['NEON_DATABASE_CONNECTION_STRING'] as string,
-    },
-  },
-});
+const prisma = new PrismaClient({});
 
 export default prisma;
