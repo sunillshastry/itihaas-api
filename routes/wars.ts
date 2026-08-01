988e07cb2f8471908f9e659f97e200cc4bd9bf53
Sunil Shastry <sunil.shastry69@gmail.com>
2026-08-01 14:54:57 -0400

    Create a new request endpoint for the wars entity at /wars/s/:search to selectively search for wars entries using various query parameters and search options

--- routes/wars.ts
+++ routes/wars.ts
@@ -2,6 +2,7 @@ import getAllWars from '@/controllers/wars/getAllWars';
 import getRandomWar from '@/controllers/wars/getRandomWar';
 import getWarById from '@/controllers/wars/getWarById';
 import getWarBySlugName from '@/controllers/wars/getWarBySlugName';
+import getWarsBySearch from '@/controllers/wars/getWarsBySearch';
 import { Router } from 'express';
 
 const warsRouter = Router();
@@ -12,6 +13,7 @@ warsRouter.get('/', getAllWars);
 warsRouter.get('/random', getRandomWar);
 
 // GET routes (with params)
+warsRouter.get('/s/:search', getWarsBySearch);
 warsRouter.get('/id/:id', getWarById);
 warsRouter.get('/:slug', getWarBySlugName);
 
